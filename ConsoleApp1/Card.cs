bc5f16f9f9eefb9716a6abe76e2f56577a53bb79
Yana <Yana.ilina.1999@list.ru>
2021-12-14 03:37:21 +0300

    Some methods

---
+++ ConsoleApp1/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Card
+    {
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public int Transactions { get; set; }
+
+        public Card(int balance, string name)
+        {
+            Balance = balance;
+            Name = name;
+        }
+
+        public void ChangeBalanceMinus(int money)
+        {
+            Balance -= money;
+            Transactions += money;
+        }
+
+        public void ChangeBalancePlus(int money)
+        {
+            Balance += money;
+            Transactions += money;
+        }
+    }
+}
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        public List<Card> Cards = new();
+        public string Name;
+        public string Login;
+
+        public User(string name, string login)
+        {
+            Name = name;
+            Login = login;
+        }
+
+    }
+}
